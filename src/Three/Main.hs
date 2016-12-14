fbece6b5e6f9ebdad818535c4bd406f5364b9b64
Shae Erisson <shae@scannedinavian.com>
2016-12-13 23:56:38 -0500

    solution for day 3 part 2

--- src/Three/Main.hs
+++ src/Three/Main.hs
@@ -5,9 +5,13 @@ import           Data.List
 
 -- part one
 triangles :: String -> [[Int]]
-triangles t = (sort . fmap read ) <$> fmap words (lines t)
+triangles t = fmap read <$> fmap words (lines t)
 
 istriangle (a:b:c:[]) =  a + b > c
 
 main = do contents <- readFile "input.txt"
-          print $ length (filter istriangle (triangles contents))
+          print $ length (filter istriangle (fmap sort $ triangles contents))
+          print $ sum $ fmap checkThree $ transpose $ triangles contents
+
+checkThree []         = 0
+checkThree (a:b:c:ds) = checkThree ds + if istriangle (sort [a,b,c]) then 1 else 0
