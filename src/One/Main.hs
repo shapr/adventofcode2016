37e9f5e89c4f562f7a110501599a09144b1d8821
Shae Erisson <shae@scannedinavian.com>
2016-12-13 22:18:09 -0500

    solver for second part of day 1

--- src/One/Main.hs
+++ src/One/Main.hs
@@ -2,8 +2,12 @@ module Main where
 
 import           Data.List.Split
 import           Data.Map.Strict hiding (foldl, map)
+import           Data.Maybe      (fromJust)
+import qualified Data.Set        as Set
 
 input = "R2, L5, L4, L5, R4, R1, L4, R5, R3, R1, L1, L1, R4, L4, L1, R4, L4, R4, L3, R5, R4, R1, R3, L1, L1, R1, L2, R5, L4, L3, R1, L2, L2, R192, L3, R5, R48, R5, L2, R76, R4, R2, R1, L1, L5, L1, R185, L5, L1, R5, L4, R1, R3, L4, L3, R1, L5, R4, L4, R4, R5, L3, L1, L2, L4, L3, L4, R2, R2, L3, L5, R2, R5, L1, R1, L3, L5, L3, R4, L4, R3, L1, R5, L3, R2, R4, R2, L1, R3, L1, L3, L5, R4, R5, R2, R2, L5, L3, L1, L1, L5, L2, L3, R3, R3, L3, L4, L5, R2, L1, R1, R3, R4, L2, R1, L1, R3, R3, L4, L2, R5, R5, L1, R4, L5, L5, R1, L5, R4, R2, L1, L4, R1, L1, L1, L5, R3, R4, L2, R1, R2, R1, R1, R3, L5, R1, R4"
+
+input' = "R8, R4, R4, R8" -- for part 2
 
 inputToTurnInt (a:as) = case a of 'R' -> (R, read as :: Int)
                                   'L' -> (L, read as :: Int)
@@ -18,17 +22,20 @@ rToE o ((L,i):as) = (left o, i)  : rToE (left o)  as
 exact = rToE North directions
 
 -- assuming N, E, S, W, add up all the values
-addDir :: (Orientation,Int) -> (Int, Int, Int, Int) -> (Int, Int, Int, Int)
-addDir (North,i) (n,e,s,w) = (n+i,e,s,w)
-addDir (East, i) (n,e,s,w) = (n,e+i,s,w)
-addDir (South,i) (n,e,s,w) = (n,e,s+i,w)
-addDir (West, i) (n,e,s,w) = (n,e,s,w+i)
+addDir ::  (Int, Int, Int, Int) -> (Orientation,Int) -> (Int, Int, Int, Int)
+addDir (n,e,s,w) (North,i) = (n+i,e,s,w)
+addDir (n,e,s,w) (East, i) = (n,e+i,s,w)
+addDir (n,e,s,w) (South,i) = (n,e,s+i,w)
+addDir (n,e,s,w) (West, i) = (n,e,s,w+i)
 
 -- subtract south from north, east from west, give the absolute value of north and east as the vector
 -- foldl (flip addDir) (0,0,0,0) exact
 total (n,e,s,w) = abs (n - s) + abs (e - w)
 
-main = print $ total $ foldl (flip addDir) (0,0,0,0) exact
+total' (n,e,s,w) = (n - s,e - w,0,0)
+
+main = do print $ total $ foldl addDir (0,0,0,0) exact
+          print $ total $ fromJust $ dup totalDirs
 
 -- handy machinery below
 data Orientation = North | East | South | West deriving (Eq, Ord, Show, Enum, Bounded)
@@ -53,4 +60,27 @@ instance Direction Orientation where
 {-
 this part is way more fun,
 must produce x,y coords for each step, and then look for the second occurence of a location
+assume we start at 0,0
+re-use addDir, but scanl instead of foldl, convert each into x,y then look for the first duplicate
 -}
+
+totalDirs :: [(Int, Int, Int, Int)]
+totalDirs = map total' $ scanl addDir (0,0,0,0) (concatMap rep exact)
+
+-- stolen from http://stackoverflow.com/a/5438809/39683
+dup :: Ord a => [a] -> Maybe a
+dup xs = dup' xs Set.empty
+  where dup' [] _ = Nothing
+        dup' (x:xs) s = if Set.member x s
+                           then Just x
+                           else dup' xs (Set.insert x s)
+
+{- the above does not work because I need to check every x,y coordinate that the path crosses,
+rather than checking each location where the path turns.
+cheesy hack: break each R8 into an exact value of Direction 1, then use the same code above
+take 8 $ repeat (Direction 1) -}
+
+rep (East ,i) = replicate i (East ,1)
+rep (North,i) = replicate i (North,1)
+rep (West ,i) = replicate i (West ,1)
+rep (South,i) = replicate i (South,1)
