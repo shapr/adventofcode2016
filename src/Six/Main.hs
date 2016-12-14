1be23f5b41b9dfb8357f2a3ffbab9f0c788060eb
Shae Erisson <shae@scannedinavian.com>
2016-12-14 12:31:38 -0500

    solve day 6 part 2

--- src/Six/Main.hs
+++ src/Six/Main.hs
@@ -7,7 +7,8 @@ byLength a b = if length a < length b then GT else LT
 chunk  :: Ord a => [a] -> [[a]]
 chunk = sortBy byLength . group . sort
 
-mostFreq s = concatMap nub $ concatMap (take 1) $ (fmap . fmap) chunk (transpose . lines) s
+freq f s = concatMap nub $ concatMap (take 1 . f) $ (fmap . fmap) chunk (transpose . lines) s
 
 main = do contents <- readFile "input.txt"
-          print $ mostFreq contents
+          print $ (freq id) contents
+          print $ (freq reverse) contents
