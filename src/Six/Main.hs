d02b666240cba699b0717f66eb81b9c011ae839b
Shae Erisson <shae@scannedinavian.com>
2016-12-17 13:15:34 -0500

    remove unnecessary parens from day six

--- src/Six/Main.hs
+++ src/Six/Main.hs
@@ -10,5 +10,5 @@ chunk = sortBy byLength . group . sort
 freq f s = concatMap nub $ concatMap (take 1 . f) $ (fmap . fmap) chunk (transpose . lines) s
 
 main = do contents <- readFile "input.txt"
-          print $ (freq id) contents
-          print $ (freq reverse) contents
+          print $ freq id contents
+          print $ freq reverse contents
