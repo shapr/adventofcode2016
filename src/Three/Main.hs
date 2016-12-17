e656e37ad16eb848eb9ca14d4657607797f24ddd
Shae Erisson <shae@scannedinavian.com>
2016-12-17 13:14:39 -0500

    minor code cleanup for day three, use <$> instead of explicit fmap

--- src/Three/Main.hs
+++ src/Three/Main.hs
@@ -3,15 +3,14 @@ module Main where
 import           Control.Applicative
 import           Data.List
 
--- part one
 triangles :: String -> [[Int]]
 triangles t = fmap read <$> fmap words (lines t)
 
 istriangle (a:b:c:[]) =  a + b > c
 
 main = do contents <- readFile "input.txt"
-          print $ length (filter istriangle (fmap sort $ triangles contents))
+          print $ length (filter istriangle (sort <$> triangles contents))
           print $ sum $ fmap checkThree $ transpose $ triangles contents
 
-checkThree []         = 0
+checkThree []         = 0 -- this function is a weird and ugly hack
 checkThree (a:b:c:ds) = checkThree ds + if istriangle (sort [a,b,c]) then 1 else 0
