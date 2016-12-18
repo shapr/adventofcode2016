f70ffce02eb23a07bf02f90bc84c889df344d2a0
Shae Erisson <shae@scannedinavian.com>
2016-12-18 15:47:48 -0500

    ugly hack solver for day 5 part 2

--- src/Five/Main.hs
+++ src/Five/Main.hs
@@ -7,6 +7,9 @@ import qualified Data.ByteString       as B hiding (foldl')
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy  as BL
 import           Data.Char             (ord)
+import           Data.List             (group, groupBy, nub, sort)
+import           GHC.Exts              (groupWith, sortWith, the)
+import           Lib                   (showHash)
 import           Numeric               (showHex)
 
 {- The eight-character password for the door is generated one character at a time by finding the MD5 hash of
@@ -18,21 +21,22 @@ zeroes. If it does, the sixth character in the hash is the next character of the
 input :: B.ByteString
 input = "uqwqemis"
 
-main = print $ map snd . take 8 $ filter fst hashes
-
-showHash :: B.ByteString -> String
-showHash = map (toEnum.fromEnum) . hexalise . B.unpack
-
-hexalise = concatMap (\c -> [ hex $ c `div` 16, hex $ c `mod` 16 ])
-        where hex i
-                | i >= 0 && i <= 9   = fromIntegral (ord '0') + i
-                | i >= 10 && i <= 15 = fromIntegral (ord 'a') + i - 10
-                | otherwise          = 0
+main = do
+  print $ map snd . take 8 $ filter fst hashes1
+  print $ uglyhack (concat $ take 15 $ groupBy (\x y -> fst x == fst y) $ (fmap snd) $ filter fst hashes2)
+    where uglyhack horrible = map snd $ take 8 $ sort $ foldl (flip mix) [] horrible
+  -- all id (elem <$> ['0'..'7'] <*> (last $ takeWhile (\x -> length x < 8) (scanl (flip mix) [] broke)))
 
 nums :: [B.ByteString]
-nums = map (BC.pack . show) [0..]
+nums = (BC.pack . show) <$> [0..]
 
-hashes :: [(Bool, Char)]
-hashes = map (isNextChar . MD5.hash) (zipWith B.append (repeat input) nums)
+-- hashes :: [(Bool, Char)]
+hashes = MD5.hash <$> (zipWith B.append (repeat input) nums)
+hashes1 = fmap isNextChar hashes
+hashes2 = fmap isNextChar' hashes
 
-isNextChar = (all (== '0') . take 5 . showHash &&& (!! 5) . showHash) . B.take 6
+isNextChar  = (all (== '0') . take 5  &&& (!! 5) ) . showHash . B.take 7
+isNextChar' = (all (== '0') . take 5 &&& ((!! 5) &&& (!! 6))) . showHash . B.take 7
+
+mix (p,c) []  = (p,c):[]
+mix (p,c) pcs = if not $ p `elem` map fst pcs then (p,c):pcs else pcs
